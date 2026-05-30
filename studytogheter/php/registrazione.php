dc2c09e02c4c447dcc0500a536618d6bd15e8c76
Simone Gentili <simone.gentili4@studio.unibo.it>
2026-05-30 15:25:21 +0200

    Aggiunta pag registrazione

---
+++ studytogheter/php/registrazione.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sing_Up - StudyTogether</title>
+        <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <header class="top-bar">
+        <h1>STUDY GROUPS</h1>
+    </header>
+
+    <main class="login-container">
+        <form class="login-box">
+
+            <input type="text" name="username" placeholder="USERNAME" required>
+
+            <input type="email" name="email" placeholder="EMAIL" required>
+
+            <input type="password" name="password" placeholder="PASSWORD" required>
+
+            <button type="submit"></button>
+
+
+            <button type="submit">REGISTRATI</button>
+        </form>
+    </main>
+</body>
+</html>
