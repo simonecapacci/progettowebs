cb194507daa290310541b7e99c1170a441243dcc
simoncino <simone.capacci3@studio.unibo.it>
2026-05-30 14:59:11 +0200

    inizio pagina login

--- studytogheter/php/login.php
+++ studytogheter/php/login.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - StudyTogether</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    
+        <header class="top-bar">
+        <h1>STUDY GROUPS</h1>
+    </header>
+
+    <main class="login-container">
+        <form class="login-box">
+            <input type="text" name="username" placeholder="EMAIL / USERNAME" required>
+
+            <input type="password" name="password" placeholder="PASSWORD" required>
+
+            <button type="submit">ACCEDI</button>
+        </form>
+    </main>
 </body>
 </html>
