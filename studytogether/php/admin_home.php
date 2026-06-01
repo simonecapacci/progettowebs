d28be4264629fe865ee3efbbab8cbd726746beac
Simone Gentili <simone.gentili4@studio.unibo.it>
2026-06-01 18:26:52 +0200

    Sistemata cosa in admin home

--- studytogether/php/admin_home.php
+++ studytogether/php/admin_home.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/bootstrap.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>StudyGroups</title>
+    <title>Admin_home</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
