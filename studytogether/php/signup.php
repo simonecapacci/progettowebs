27b952bc4efdfe6944b684c1c9de39de34ac6c3b
Simone Gentili <simone.gentili4@studio.unibo.it>
2026-06-03 15:45:06 +0200

    Rimosso signup.css

--- studytogether/php/signup.php
+++ studytogether/php/signup.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/bootstrap.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up - StudyTogether</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/signup.css">
+    <link rel="stylesheet" href="../css/style.css">
 
 </head>
 <body>
