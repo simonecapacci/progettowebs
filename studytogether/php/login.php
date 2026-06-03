756bfbe3926d0d4659708c97e2e30293339f01a5
simoncino <simone.capacci3@studio.unibo.it>
2026-06-03 15:32:21 +0200

    modifiche login bottoni

--- studytogether/php/login.php
+++ studytogether/php/login.php
@@ -45,7 +45,7 @@ require_once __DIR__ . '/bootstrap.php';
                             ACCEDI
                         </h2>
 
-                        <form action="signup.php" method="post">
+                        <form action="home.php" method="post">
                             
                             <div class="mb-3">
                                 <label for="email" class="form-lable">
@@ -75,11 +75,10 @@ require_once __DIR__ . '/bootstrap.php';
                             non hai un account?
                         </p>
 
-                        <form action="signup.php" method="get">
-                            <button type="button" class="btn btn-outline-primary w-100">
+                        
+                        <a href="signup.php" class="btn btn-outline-primary w-100">
                                 Registrati
-                            </button>
-                        </form>
+                        </a>
 
                     </div>
                 </div>
