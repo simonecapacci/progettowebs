3234e4dfd1efd4ef940147896f10d430dcc1d2c3
Enrico Casadei <enrico.casadei18@studio.unibo.it>
2026-06-04 17:14:11 +0200

    Fix errore grafico in admin_home

--- studytogether/php/search_group.php
+++ studytogether/php/search_group.php
@@ -20,7 +20,6 @@ if (isset($dbh) && method_exists($dbh, 'getGroups')) {
     <title>Cerca gruppo - StudyGroups</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/components.css">
 </head>
 <body class="search-page bg-body-tertiary d-flex flex-column min-vh-100">
     <?php require_once 'navbar.php'; ?>
