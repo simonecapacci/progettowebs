d0d81a886a0c78859ac53a0acfd961317ddb7d5d
Enrico Casadei <enrico.casadei18@studio.unibo.it>
2026-06-06 12:51:26 +0200

    Aggiunto reindirizzamento a index.php in caso di accesso senza credenziali

--- studytogether/php/bootstrap.php
+++ studytogether/php/bootstrap.php
@@ -8,6 +8,14 @@ session_set_cookie_params([
 
 session_start();
 
+$currentPage = basename($_SERVER['SCRIPT_NAME'] ?? '');
+$publicPages = ['index.php', 'login.php', 'signup.php'];
+
+if (!isset($_SESSION['user_id']) && !in_array($currentPage, $publicPages, true)) {
+    header('Location: index.php');
+    exit;
+}
+
 require_once __DIR__ . "/db/database.php";
 
 $dbh = new DatabaseHelper("localhost", "root", "", "studytogether", 1104);
