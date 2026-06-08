da0ffdd76179fb8450ac0344f036948e80077e21
Enrico Casadei <enrico.casadei18@studio.unibo.it>
2026-06-08 18:36:20 +0200

    Rimozione file inutili

--- studytogether/php/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer bg-primary text-white text-center py-3">
-    <div class="container">
-        <p class="mb-0">
-            © <?php echo date('Y'); ?> StudyGroups
-        </p>
-    </div>
-</footer>
