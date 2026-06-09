4533b50273d11e766746dfa5304c30870a3db25e
Simone28Gent <simone.gentili4@studio.unibo.it>
2026-06-09 12:16:38 +0200

    cambiato nome progetto

--- studygroups/login.php
+++ studygroups/login.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/api/api-login.php';
 
-$templateParams['content_page'] = './template/auth_page.php';
+$templateParams['content_page'] = './template/auth_card.php';
 $templateParams['pageTitle'] = 'Login';
 
 $title = 'Accedi';
--- studygroups/signup.php
+++ studygroups/signup.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/api/api-signup.php';
 
 
 $templateParams['pageTitle'] = 'Sign Up';
-$templateParams['content_page'] = './template/auth_page.php';
+$templateParams['content_page'] = './template/auth_card.php';
 
 $title = 'Registrati';
 $action = 'signup.php';
